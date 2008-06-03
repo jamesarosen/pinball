a1a12d56d31dc9cadf0e8cd0ceaa7406a4db16a0
James Rosen <james@u-presence.com>
2008-06-03 14:49:01 -0400

    added db/ and log/; moved .gitignores for those directories into the directories themselves

---
+++ db/migrate/20080526223738_alpha.rb
@@ -0,0 +1,44 @@
+class Alpha < ActiveRecord::Migration
+  def self.up
+    create_table 'users', :force => true do |t|
+      t.string    'login',                    :limit => 40, :nil => false
+      t.string    'crypted_password',         :limit => 40, :nil => false
+      t.string    'salt',                     :limit => 40, :nil => false
+      t.string    'remember_token'
+      t.datetime  'remember_token_expires_at'
+      t.boolean   'is_admin',                 :default => false
+      t.string    'email_verification'
+      t.boolean   'email_verified',           :default => false
+      t.timestamps
+    end
+
+    add_index 'users', ['login'], :name => 'index_users_on_login'
+    
+    create_table 'profiles', :force => true do |t|
+      t.integer   'user_id'
+      t.string    'display_name'
+      t.text      'description'
+      t.string    'email',                    :nil => false
+      t.string    'cell_number'
+      t.string    'cell_carrier'
+      t.integer   'current_location'
+      t.timestamps
+    end
+
+    add_index "profiles", "user_id"
+
+    create_table :locations do |t|
+      t.string    'display_name',             :nil => false
+      t.string    'type',                     :nil => false
+      t.decimal   'latitude',                 :precision => 11, :scale => 9  #  xy.abcdefghi
+      t.decimal   'longitude',                :precision => 12, :scale => 9  # xyz.abcdefghi
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :locations
+    drop_table :profiles
+    drop_table :users
+  end
+end
