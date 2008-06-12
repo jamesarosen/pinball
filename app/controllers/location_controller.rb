981fcb1ce359fa027e5464df7e38b19f5634de1a
James Rosen <james@u-presence.com>
2008-06-12 12:40:53 -0400

    added CRUD operations to LocationController; added tests

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,24 +1,39 @@
 class LocationController < ApplicationController
 
+  requires_profile :only => [ :current ]
   requires_is_self :except => [ :current ]
   
   # GET only
   def current
+    @location = requested_profile.location || 'Nowhere'
   end
   
   # GET only
   def edit
+    @location = requested_profile.location
   end
 
   # POST only
   def update
-    new_location = params[:location]
-    flash[:notice] = "Updated your location to #{new_location}"
-    redirect_to :action => 'whos_around'
+    requested_profile.location = params[:location]
+    requested_profile.save!
+    current_user.reload
+    flash[:notice] = "Updated your location to #{requested_profile.location}"
+    if requested_profile.location
+      redirect_to :action => 'whos_around'
+    else
+      redirect_to :controller => 'profiles', :action => 'dashboard', :profile_id => current_user
+    end
   end
 
   # GET only
   def whos_around
+    if requested_profile.location.nil?
+      flash[:notice] = "Please set your current location to see who's around"
+      redirect_to :action => 'edit'
+    else
+      @whos_around = requested_profile.find_nearby(:include_self => false)
+    end
   end
 
 end
--- test/functional/location_controller_test.rb
+++ test/functional/location_controller_test.rb
@@ -23,20 +23,40 @@ class LocationControllerTest < ActionController::TestCase
     
     should_be_allowed 'to view her own location' do
       get :current, :profile_id => current_user
+      assert_equal locations(:new_york_ny), assigns(:location)
     end
     
     should_be_allowed 'to view another user\'s location' do
       get :current, :profile_id => somebody_other_than(current_user)
+      assert_not_nil assigns(:location)
     end
     
     should_be_allowed 'to see the form to change her location' do
       get :edit, :profile_id => current_user
     end
     
-    should 'be able to change her location' do
+    should 'be able to set her location to Nowhere' do
       post :update, :profile_id => current_user
       assert_response :redirect
+      assert_redirected_to :controller => 'profiles', :action => 'dashboard'
+    end
+    
+    should 'be able to change her location' do
+      @houston = locations(:houston_tx)
+      Location.expects(:parse).with('anywhere').returns(@houston)
+      
+      post :update, :profile_id => current_user, :location => 'anywhere'
+      assert_response :redirect
       assert_redirected_to :action => 'whos_around'
+      assert_equal @houston, current_user.profile.location
+    end
+    
+    should 'not be able to change her location to an unparseable string' do
+      old_location = current_user.profile.location
+      Location.expects(:parse).with('anywhere').raises(Location::ParseError.new('foo'))
+      post :update, :profile_id => current_user, :location => 'anywhere'
+      assert_template 'location/edit'
+      assert_equal old_location, current_user.profile.location
     end
     
     should_be_forbidden 'from changing another user\'s location' do
@@ -45,6 +65,8 @@ class LocationControllerTest < ActionController::TestCase
     
     should 'be able to see who\'s around' do
       get :whos_around, :profile_id => current_user
+      assert_not_nil assigns(:whos_around)
+      assert assigns(:whos_around).kind_of?(Array)
     end
     
     should 'be forbidden from seeing who\'s around another user' do
