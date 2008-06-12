dd1e7247bd38de4e86ca62f4c266e6f50c22e47e
James Rosen <james@u-presence.com>
2008-06-11 23:35:46 -0400

    added support for calling define_once outside of the scope of a class (really a module)

--- config/initializers/define_once.rb
+++ config/initializers/define_once.rb
@@ -2,4 +2,10 @@ class Module
   def define_once(sym, value)
     const_set(sym, value) unless const_defined?(sym)
   end
-end 
+end
+
+class Object
+  def define_once(sym, value)
+    self.class.define_once(sym, value)
+  end
+end
