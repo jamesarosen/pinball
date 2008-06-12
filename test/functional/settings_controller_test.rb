8c6a1ff23c8f518ba9a9930509e4569ccd568567
James Rosen <james@u-presence.com>
2008-06-12 14:28:34 -0400

    forgot to include relevant tests in previous commit

--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -6,16 +6,16 @@ class SettingsControllerTest < ActionController::TestCase
     
     context 'A guest' do
       should_be_unauthorized 'to view a user\'s privacy settings' do
-        get :privacy, :profile_id => anybody
+        get :edit_privacy, :profile_id => anybody
       end
       should_be_unauthorized 'to change a user\'s privacy settings' do
-        post :privacy, :profile_id => anybody
+        post :update_privacy, :profile_id => anybody
       end
       should_be_unauthorized 'to view a user\'s notification settings' do
-        get :notifications, :profile_id => anybody
+        get :edit_notifications, :profile_id => anybody
       end
       should_be_unauthorized 'to change a user\'s notification settings' do
-        post :notifications, :profile_id => anybody
+        post :update_notifications, :profile_id => anybody
       end
     end
   
@@ -25,30 +25,30 @@ class SettingsControllerTest < ActionController::TestCase
       end
       
       should_be_forbidden 'from viewing another user\'s privacy settings' do
-        get :privacy, :profile_id => somebody_other_than(current_user)
+        get :edit_privacy, :profile_id => somebody_other_than(current_user)
       end
       should_be_forbidden 'from changing another user\'s privacy settings' do
-        post :privacy, :profile_id => somebody_other_than(current_user)
+        post :update_privacy, :profile_id => somebody_other_than(current_user)
       end
       should_be_forbidden 'from viewing another user\'s notification settings' do
-        get :notifications, :profile_id => somebody_other_than(current_user)
+        get :edit_notifications, :profile_id => somebody_other_than(current_user)
       end
       should_be_forbidden 'from changing another user\'s notification settings' do
-        get :notifications, :profile_id => somebody_other_than(current_user)
+        get :edit_notifications, :profile_id => somebody_other_than(current_user)
       end
       
       should_be_allowed 'to view his privacy settings' do
-        get :privacy, :profile_id => current_user
+        get :edit_privacy, :profile_id => current_user
       end
       should_be_allowed 'to change his privacy settings' do
-        post :privacy, :profile_id => current_user
+        post :update_privacy, :profile_id => current_user
         assert_response :success
       end
       should_be_allowed 'to view his notification settings' do
-        get :notifications, :profile_id => current_user
+        get :edit_notifications, :profile_id => current_user
       end
       should_be_allowed 'to change his notification settings' do
-        post :notifications, :profile_id => current_user
+        post :update_notifications, :profile_id => current_user
       end
     end
 
