33511b0179024fd395cfc618f6d353faf05e6bf7
James Rosen <james@u-presence.com>
2008-06-18 22:32:49 -0400

    added debug stylesheet conditional on development (and currently off)

--- app/controllers/utilities/controller/environment.rb
+++ app/controllers/utilities/controller/environment.rb
@@ -2,16 +2,23 @@ module Utilities
   module Controller
     module Environment
       
-      def development?
-        ENV['RAILS_ENV'] == 'development'
+      def self.included(base)
+        base.send :include, Utilities::Controller::Environment::InstanceMethods
+        base.helper_method :development?, :test?, :production?
       end
       
-      def test?
-        ENV['RAILS_ENV'] == 'test'
-      end
+      module InstanceMethods
+        def development?
+          ENV['RAILS_ENV'] == 'development'
+        end
       
-      def production?
-        !development && !test
+        def test?
+          ENV['RAILS_ENV'] == 'test'
+        end
+      
+        def production?
+          !development && !test
+        end
       end
       
     end
