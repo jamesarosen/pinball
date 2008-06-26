d5cb87ee92170c87ec8d4fb5885e770879798526
James Rosen <james.a.rosen@gmail.com>
2008-06-26 10:44:53 -0400

    fixed bug in hash block and pass

--- config/initializers/hash.rb
+++ config/initializers/hash.rb
@@ -3,14 +3,14 @@
 Hash.class_eval do
   
   def pass(*keys)
-    tmp = self.dup
-    tmp.delete_if {|k,v| ! tmp.has_key?(k) }
+    tmp = self.dup.clear   # so we get whatever class self is
+    keys.each { |k| tmp[k] = self[k] if self.has_key?(k) }
     tmp
   end
   
   def block(*keys)
     tmp = self.dup
-    tmp.delete_if {|k,v| tmp.has_key?(k) }
+    keys.each { |k| tmp.delete(k) }
     tmp
   end
   
